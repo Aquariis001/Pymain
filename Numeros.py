06a39bc0b4c67f7f1e702dc3ebe66196f5a39911
Willia Santos <dennys.g7@icloud.com>
2023-02-14 16:03:55 -0300

    fix the '#'

--- Numeros.py
+++ Numeros.py
@@ -1,12 +1,14 @@
-#Existem três tipos de numeros no Python sendo eles INT, FLOAT e COMPLEX
-
+'''
+Existem três tipos de numeros no Python sendo eles INT, FLOAT e COMPLEX
+'''
 a = int(7)
-
 b = float(7)
 
 c = complex(7)
 
-#Usando os numeros na prática
+'''
+Usando os numeros na prática
+'''
 
 print(a)
 
--- Type.py
+++ Type.py
@@ -1,12 +1,18 @@
-# Type verifica qual é o tipo da sua variavel.
+'''
+Type verifica qual é o tipo da sua variavel.
+'''
 
 a = int(45)
 b = float(45)
 c = str(45)
 
-# Usando Type na prática
+'''
+Usando Type na prática
+'''
 print(type(a))
+
 print(type(b))
+
 print(type(c))
 
 
@@ -19,4 +25,3 @@ x = 2
 
 if x == 3:
     print("Vamos", x)
-    
