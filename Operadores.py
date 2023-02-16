c6ee24c032c5edff2c7fcf8eaa6a5a6988c7f18b
Willia Santos <dennys.g7@icloud.com>
2023-02-16 19:48:47 -0300

    Operations

---
+++ Operadores.py
@@ -0,0 +1,26 @@
+"""
+
+    Operadores Aritméticos
+
+"""
+
+nome_long = 8
+x = 5
+y = 3
+
+""" Operador de atribuição """
+
+"""
+    Operador da soma/adição +
+    Operador da subtrção -
+    Operador da multiplicação *
+    Operador divisão /
+
+    """
+
+a = 7 + 3
+b = 15 - 10
+c = 4 * 4
+d = 62 / 2
+
+print(a, b, c, d)
