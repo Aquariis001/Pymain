6bfbcc1c3cc01669933958bbb4a1456bf796d8dd
Willia Santos <dennys.g7@icloud.com>
2023-03-01 20:11:58 -0300

    Troca de Variavel

---
+++ Exercice03.py
@@ -0,0 +1,17 @@
+"""
+Exercício - Trocar Vaiáveis
+"""
+
+# Trocando variaveis em python
+
+x = input("Insira o valor de x: ")
+y = input("Insira o valor de y: ")
+
+# Criaremos uma variavel temporaria
+
+temp = x
+x = y
+y = temp
+
+print(f"O valor de x depois da troca: {x}")
+print(f"O valor de x depois da troca: {y}")
