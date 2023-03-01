44f8e4c2a2feec1804a53ea97199c22fe9733aeb
Willia Santos <dennys.g7@icloud.com>
2023-03-01 20:48:33 -0300

    impa ou par

---
+++ impar_par.py
@@ -0,0 +1,18 @@
+"""
+Como descobrir se um número é impar ou par
+
+
+num = 5
+numero = 2
+
+print(5/2)
+
+# numeros pares 0,2,4,6,8,10,12,14...
+"""
+print(30 * "-=")
+numero = int(input("Insira um numero Par: "))
+if (numero % 2) == 0:
+    print(f'{numero} é um numero Par')
+else:
+    print(f'{numero} é um numero Impar')
+print(30 * "-=")
