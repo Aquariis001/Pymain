c4ee9ac3c93960fc48967faf7daa30b27634b19a
Willia Santos <dennys.g7@icloud.com>
2023-03-01 14:54:40 -0300

    While

--- while.py
+++ while.py
@@ -10,8 +10,46 @@ do while*
 
 """
 
+# For
+
+"""
+
 a = 0
 
 while a <= 10:
+    print(a <= 10)
     print(a)
-    a = a + 1 
+    a = a + 1
+else:
+    print(f'a não é menor ou igual a 5. Valor de a: {a}')
+
+"""
+"""
+
+# For
+
+s = 'sergipe'
+
+for x in s:
+    print(x)
+    
+"""
+
+"""
+for x in range(6): #range(x) imprimi a contagem em até o determinado valor
+    print(x)
+"""
+"""
+for x in range(5,8):
+    print(x)
+"""
+"""
+for x in range(2, 13, 5): # range()
+    print(x)
+"""
+'''
+for x in range(6):
+    print(x)
+else:
+    print('Chegamos ao fim')
+'''
