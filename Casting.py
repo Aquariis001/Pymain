65cf74b34799fe48746f31f067808aaf7c35d480
Willia Santos <dennys.g7@icloud.com>
2023-03-03 08:14:50 -0300

    fix Casting

--- Casting.py
+++ Casting.py
@@ -1,21 +1,21 @@
-#Casting converte os dados de textos ou numeros
+# Casting converte os dados de textos ou numeros
 
 a = int(22)
 b = int(2.2)
 c = int('22')
 
-#Usando Casting na prática
+# Usando Casting na prática
 
-print (a, b, c)
+print(a, b, c)
 
 x = float(22)
 w = float(2.2)
 z = float('22')
 
-print (z, w, x)
+print(z, w, x)
 
 m = complex(22)
 n = complex(2.2)
 o = complex('22')
 
-print (m, n, o)
+print(m, n, o)
